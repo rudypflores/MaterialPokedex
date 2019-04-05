18cc8b027d9d61c934801be3635463a1974e9d10
rudypflores <rudypichola@gmail.com>
2019-04-05 10:34:38 -0500

    Updates

---
+++ precache-manifest.1af872fa1f2f5a63fb312b06b3354738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a63875a03ac84aa327c0",
+    "url": "/MaterialPokedex/static/js/runtime~main.d26cb06b.js"
+  },
+  {
+    "revision": "f1ccf1c177cf2021a3ca",
+    "url": "/MaterialPokedex/static/js/main.b6c97132.chunk.js"
+  },
+  {
+    "revision": "a36533ae65938741f383",
+    "url": "/MaterialPokedex/static/js/2.4f017e5b.chunk.js"
+  },
+  {
+    "revision": "f1ccf1c177cf2021a3ca",
+    "url": "/MaterialPokedex/static/css/main.ee8d0019.chunk.css"
+  },
+  {
+    "revision": "78bf369f692692c5e082ff02a5fb7996",
+    "url": "/MaterialPokedex/index.html"
+  }
+];
